9b2e7831e6ef8ce945611fcf7d8d7cd26fa30bd2
Hieunmb <zhuongdae@gmail.com>
2023-04-13 16:33:26 +0700

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World!");//print
+        int x = 10;
+        string s = "T2207A";
+        double pi = 3.14159;
+        if(x>5)
+        {
+            x--;
+        }
+        else
+        {
+            x++;
+        }
+        int[] arr=new int[5];
+        arr[0] = 1;
+        arr[1] = 15;
+        arr[2] = 9;
+        arr[3] = 9;
+        arr[4] = -16;
+        foreach(int i in arr)
+        {
+            Console.WriteLine(i);
+        }
+        string cmd= Console.ReadLine();
+        Console.WriteLine("Chuoi vua nhap:"+cmd);
+        string cmd2= Console.ReadLine();
+        int n=Convert.ToInt32(cmd2);
+        Console.WriteLine("So vua nhap:" + n);
+        if(isPrimed(n)) 
+        {
+            Console.WriteLine(n + "La so nguyen to");
+        }
+    }
+    public static bool isPrimed(int n)
+    {
+        if (n < 2) return false;
+        if (n < 4) return true;
+        for(int i=2;i<=n;i++)
+        {
+            if(n%i==0) return false;
+        }
+    }
+}
